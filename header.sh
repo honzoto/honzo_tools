9cf8dc5cb3228d7b4c798861276242fbd8d58558
Hahn <57220564+honzoto@users.noreply.github.com>
2022-08-24 18:41:26 -0400

    header.sh
    Welcome to honzo_tools repo, where you will find random scripts that do random things.

---
+++ header.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+red=`tput setaf 1`
+honzo=`tput setaf 3`
+outer=`tput setaf 2`
+reset=`tput sgr0`
+# 
+bold=`tput bold`
+
+echo ""
+echo ""
+echo -e "     ${outer}#####################################################################   ${reset}  "
+echo -e "     ${outer}##                                                                 ##   ${reset}  "
+echo -e "     ${outer}##${honzo}\e[5m   ||    ||     //====\\\\\\\\     |\\    ||    ======//=    //====\\\\\\\\   \e[25m${outer}##${reset}     "
+echo -e "     ${outer}##${honzo}\e[5m   ||    ||     ||    ||     |\\\\\\\\   ||         //      ||    ||   \e[25m${outer}##${reset}     "
+echo -e "     ${outer}##   ${honzo}\e[5m||====||     || \e[25m${reset}${bold}[ WELCOME TO HAHN'S PIPELINE ]     ${reset}${honzo}\e[5m||    ||   \e[25m${outer}##${reset}     "
+echo -e "     ${outer}##${honzo}\e[5m   ||    ||     ||    ||     || \\\\\\\\ ||      //         ||    ||   \e[25m${outer}##${reset}     "
+echo -e "     ${outer}##${honzo}\e[5m   ||    ||     \\\\\\\\====//     ||  \\\\\\\\||    =//======    \\\\\\\\====//   \e[25m${outer}##${reset}     "
+echo -e "     ${outer}##                                                                 ${outer}##   ${reset}  "
+echo -e "     ${outer}#####################################################################   ${reset}   "
+echo ""
+echo ""
